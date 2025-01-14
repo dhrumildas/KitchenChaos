5b90fa5cf7a7d508175aa68485cf04b063e0d009
dhrumildas <dhrumildas1@gmail.com>
2025-01-15 02:07:51 +0530

    Commit before creating the Delivery System

--- Assets/New_assets/Scripts/DeliveryManager.cs
+++ Assets/New_assets/Scripts/DeliveryManager.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
+    [SerializeField] private MainListSO recipeListSO;
     private List<RecipeListSO> waitingRecipeSOList;
+
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+
+    private void Awake()
+    {
+        waitingRecipeSOList = new List<RecipeListSO>();
+    }
+    private void Update()
+    {
+        spawnRecipeTimer -= Time.deltaTime;
+        if (spawnRecipeTimer <= 0f)
+        {
+            spawnRecipeTimer = spawnRecipeTimerMax;
+
+            if (waitingRecipeSOList.Count < waitingRecipesMax)
+            {
+                RecipeListSO waitingRecipeSO = recipeListSO.mainRecipeSOList[Random.Range(0, recipeListSO.mainRecipeSOList.Count)];
+                Debug.Log(waitingRecipeSO.recipeName);
+                waitingRecipeSOList.Add(waitingRecipeSO);
+            }
+        }
+    }
 }

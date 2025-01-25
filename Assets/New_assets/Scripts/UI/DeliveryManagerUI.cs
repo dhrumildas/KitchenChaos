71cfefc593a1c7346a36eed3c9e9bb3e86e0ab30
dhrumildas <dhrumildas1@gmail.com>
2025-01-25 15:17:29 +0530

    Final DdeliveryManager (Fixed)

--- Assets/New_assets/Scripts/UI/DeliveryManagerUI.cs
+++ Assets/New_assets/Scripts/UI/DeliveryManagerUI.cs
@@ -12,6 +12,23 @@ public class DeliveryManagerUI : MonoBehaviour
         recipeTemplate.gameObject.SetActive(false);
     }
 
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSpawned += DeliveryManager_OnRecipeSpawned;
+        DeliveryManager.Instance.OnRecipeCompleted += DeliveryManager_OnRecipeCompleted;
+        UpdateVisual();
+    }
+
+    private void DeliveryManager_OnRecipeCompleted(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void DeliveryManager_OnRecipeSpawned(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+    }
+
     private void UpdateVisual()
     {
         foreach (Transform child in container)
@@ -19,10 +36,11 @@ public class DeliveryManagerUI : MonoBehaviour
             if(child == recipeTemplate) continue;
             Destroy(child.gameObject);
         }
-        foreach (DeliveryManager.Instance.GetWaitingRecipeSOList())
+        foreach (MenuSO menuSO in DeliveryManager.Instance.GetWaitingRecipeSOList())
         {
             Transform recipeTransform = Instantiate(recipeTemplate, container);
             recipeTransform.gameObject.SetActive(true);
+            recipeTransform.GetComponent<DeliveryManagerSingleUI>().SetRecipeSO(menuSO);
         }
     }
 }
